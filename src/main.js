81ce55e86f662de64a4fc02d0294c6a235645b74
MeowKJ <ijink@qq.com>
2024-01-11 18:07:50 +0800

    添加vercel分析

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import router from './router'; // 导入创建的路由实例
 import './style.css'
 import App from './App.vue'
 
+import {inject} from '@vercel/analytics';
+
+inject();  // Vercel 分析
 
 createApp(App).use(router).mount('#app');
